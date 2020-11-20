65b2c2ab03e7ad628285c6f4d03d6e4f3b0af7de
John Shaffer <johns@gatea.com>
2020-11-20 08:38:11 -0500

    gzip

--- scripts/UC.bash
+++ scripts/UC.bash
@@ -2,14 +2,25 @@
 
 NROW=60
 SKIP=09:29:45
-DATE=20201102
 XTRA="-o Capacity,GIF,ARCAB,ICE"
-
+DATE=`ls | grep 2020 | head -1`
 clear
+#
+# Tapes
+#
+DIR='Tapes : '
+for dir in `ls | grep 2020`; do
+   DIR="${DIR} ${dir}"
+done
+echo ${DIR}
+#
+# Input from user w/ defaults
+#
 read -p "Date [${DATE}]: " dt
 read -p "Skip [${SKIP}]: " sk
 dt=${dt:-${DATE}} ; DATE=${dt}
 sk=${sk:-${SKIP}} ; SKIP=${sk}
 FILE=./${DATE}/mon/UC-quodd173.out.gz
 
+## echo "./py/DumpMon.py ${FILE} -rows ${NROW} ${XTRA} -s ${SKIP}"; sleep 1
 ./py/DumpMon.py ${FILE} -rows ${NROW} ${XTRA} -s ${SKIP}
--- scripts/chic.bash
+++ scripts/chic.bash
@@ -3,13 +3,24 @@
 NROW=60
 TYPE=RecordUDP
 SKIP=09:29:45
-DATE=20201102
-
+DATE=`ls | grep 2020 | head -1`
 clear
+#
+# Tapes
+#
+DIR='Tapes : '
+for dir in `ls | grep 2020`; do
+   DIR="${DIR} ${dir}"
+done
+echo ${DIR}
+#
+# Input from user w/ defaults
+#
 read -p "Date [${DATE}]: " dt
 read -p "Skip [${SKIP}]: " sk
 dt=${dt:-${DATE}} ; DATE=${dt}
 sk=${sk:-${SKIP}} ; SKIP=${sk}
 FILE=./${DATE}/mon/${TYPE}-qchic209.out.gz
 
+## echo "./py/DumpMon.py ${FILE} -rows ${NROW} ${XTRA} -s ${SKIP}" ; sleep 1
 ./py/DumpMon.py ${FILE} -rows ${NROW} ${XTRA} -s ${SKIP}
